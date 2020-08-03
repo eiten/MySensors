c76d59cf87ad4d789cf3a6603f373b14f0858c21
Eduard Iten <eduard@iten.pro>
2020-08-03 23:37:47 +0200

    Added architecture ASR650x

--- MySensors.h
+++ MySensors.h
@@ -76,6 +76,9 @@
 #elif defined(__arm__) && defined(TEENSYDUINO)
 #include "hal/architecture/Teensy3/MyHwTeensy3.cpp"
 #include "hal/crypto/generic/MyCryptoGeneric.cpp"
+#elif defined(__ASR6501__) || defined (__ASR6502__)
+#include "hal/architecture/ASR650x/MyHwASR650x.cpp"
+#include "hal/crypto/generic/MyCryptoGeneric.cpp"
 #elif defined(__linux__)
 #include "hal/architecture/Linux/MyHwLinuxGeneric.cpp"
 #include "hal/crypto/generic/MyCryptoGeneric.cpp"
@@ -321,7 +324,7 @@ MY_DEFAULT_RX_LED_PIN in your sketch instead to enable LEDs
 #define MY_RAM_ROUTING_TABLE_ENABLED
 #elif defined(MY_RAM_ROUTING_TABLE_FEATURE) && defined(MY_REPEATER_FEATURE)
 // activate feature based on architecture
-#if defined(ARDUINO_ARCH_ESP8266) || defined(ARDUINO_ARCH_ESP32) || defined(ARDUINO_ARCH_SAMD) || defined(ARDUINO_ARCH_NRF5) || defined(ARDUINO_ARCH_STM32F1) || defined(TEENSYDUINO) || defined(__linux__)
+#if defined(ARDUINO_ARCH_ESP8266) || defined(ARDUINO_ARCH_ESP32) || defined(ARDUINO_ARCH_SAMD) || defined(ARDUINO_ARCH_NRF5) || defined(ARDUINO_ARCH_STM32F1) || defined(TEENSYDUINO) || defined(__linux__) || #elif defined(__ASR6501__) || defined (__ASR6502__)
 #define MY_RAM_ROUTING_TABLE_ENABLED
 #elif defined(ARDUINO_ARCH_AVR)
 #if defined(__avr_atmega1280__) || defined(__avr_atmega1284__) || defined(__avr_atmega2560__)
@@ -442,12 +445,15 @@ MY_DEFAULT_RX_LED_PIN in your sketch instead to enable LEDs
 #include "hal/architecture/NRF5/MyMainNRF5.cpp"
 #elif defined(ARDUINO_ARCH_ESP32)
 #include "hal/architecture/ESP32/MyMainESP32.cpp"
+#elif defined(ARDUINO_ARCH_ASR650x)
+#include "hal/architecture/ASR650x/MyMainASR650x.cpp"
 #elif defined(__linux__)
 #include "hal/architecture/Linux/MyMainLinuxGeneric.cpp"
 #elif defined(ARDUINO_ARCH_STM32F1)
 #include "hal/architecture/STM32F1/MyMainSTM32F1.cpp"
 #elif defined(__arm__) && defined(TEENSYDUINO)
 #include "hal/architecture/Teensy3/MyMainTeensy3.cpp"
+
 #endif
 
 #endif
---
+++ hal/architecture/ASR650x/MyHwASR650x.h
@@ -0,0 +1,83 @@
+/**
+* The MySensors Arduino library handles the wireless radio link and protocol
+* between your home built sensors/actuators and HA controller of choice.
+* The sensors forms a self healing radio network with optional repeaters. Each
+* repeater and gateway builds a routing tables in EEPROM which keeps track of the
+* network topology allowing messages to be routed to nodes.
+*
+* Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
+* Copyright (C) 2013-2020 Sensnology AB
+* Full contributor list: https://github.com/mysensors/MySensors/graphs/contributors
+*
+* Documentation: http://www.mysensors.org
+* Support Forum: http://forum.mysensors.org
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* version 2 as published by the Free Software Foundation.
+*
+* Radio wiring Teensy3.x: RF24, RFM69, RFM95:
+* MISO	12
+* MOSI	11
+* SCK	13
+* CSN	10
+* CE	9 (RF24)
+* IRQ	8 (opt. RF24, RFM69, RFM95)
+*/
+
+#ifndef MyHwASR650x_h
+#define MyHwASR650x_h
+
+#include <SPI.h>
+#include "atomic"
+#include "board-config.h"
+
+#ifdef __cplusplus
+#include <Arduino.h>
+#endif
+
+#define CRYPTO_BIG_ENDIAN
+
+#ifndef MY_SERIALDEVICE
+#define MY_SERIALDEVICE Serial
+#endif
+
+#ifndef MY_DEBUGDEVICE
+#define MY_DEBUGDEVICE MY_SERIALDEVICE
+#endif
+
+// Define these as macros to save valuable space
+#define hwDigitalWrite(__pin, __value) digitalWrite(__pin, __value)
+#define hwDigitalRead(__pin) digitalReadFast(__pin)
+#define hwPinMode(__pin, __value) pinMode(__pin, __value)
+#define hwMillis() millis()
+#define hwGetSleepRemaining() (0ul)
+
+void hwRandomNumberInit(void);
+bool hwInit(void);
+void hwWatchdogReset(void);
+void hwReboot(void);
+
+uint8_t hwReadConfig(const int pos);
+void hwWriteConfig(const int pos, uint8_t value);
+void hwReadConfigBlock(void *buffer, const void *pos, size_t length);
+void hwWriteConfigBlock(void *buffer, const void *pos, size_t length);
+
+
+// SOFTSPI
+#ifdef MY_SOFTSPI
+#error Soft SPI is not available on this architecture!
+#endif
+#define hwSPI SPI //!< hwSPI
+
+#define MY_HW_HAS_GETENTROPY
+
+#define SS RADIO_NSS
+
+#ifndef digitalPinToInterrupt
+#define digitalPinToInterrupt(__pin) (__pin)
+#endif
+
+#define yield()
+
+#endif
